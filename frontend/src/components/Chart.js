ffc67cf064af30e6e33a30cc989bc87e3a2ace56
Luis Ladino <ladino72@hotmail.com>
2021-05-20 15:02:33 -0500

    Deployment

--- frontend/src/components/Chart.js
+++ frontend/src/components/Chart.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {Bar } from "react-chartjs-2";
+import {HorizontalBar} from 'react-chartjs-2';
 
 //https://codesandbox.io/s/jjpk3o5l3?file=/src/Hello.js:52-71
 const Chart = ({ chartDatos}) => {
@@ -13,17 +13,22 @@ const Chart = ({ chartDatos}) => {
 
     for (let p = 0; p < data.length; p++) {
         id[p] = data[p].id;
-        Points[p] = data[p].Points
+        Points[p] = data[p].Points.toFixed(2);
     }
         const chart = () => {
             setChartData({
                 labels: id,
+                
                 datasets: [
                     {
                         label: topic,
                         data: Points,
-                        backgroundColor: ["rgba(75, 192, 192, 0.6)"],
-                        borderWidth: 4
+                        backgroundColor: "rgba(75, 192, 192, 0.6)",
+                        borderColor: "rgba(70, 195, 195, 1.0)",
+                        borderWidth: 2,
+                        borderRadius:50,
+                        hoverBackgroundColor:"#17a2b8",
+                        hoverBorderColor:"#17a2b8"
                     }
                 ]
             });
@@ -33,7 +38,7 @@ const Chart = ({ chartDatos}) => {
 
     return (
         <div >
-            <Bar data={chartData} options={{ responsive: true }} />
+            <HorizontalBar data={chartData} options={{ responsive: true }} />
         </div>
     )
 }
